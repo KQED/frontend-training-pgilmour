d2cb5172c4da398753ae4e3e1109a4fab82d6a9f
Pamela Gilmour <pgilmour@kqed.org>
2022-07-18 15:35:00 -0700

    feat(app): Build out React starter code

---
+++ react-app/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+    return (
+      <div className="App">
+          <p>
+            Hello there, from App.js
+          </p>
+      </div>
+    );
+  }
+  
+  export default App;
