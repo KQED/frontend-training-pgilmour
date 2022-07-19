7abc73e9201cfb1b53f5c853c6b1061ddc44ceed
Pamela Gilmour <pgilmour@kqed.org>
2022-07-19 13:42:23 -0700

    chore(app): Clean up relative pathways and code accordingly

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
 console.log('Hello from my React app')
+
+const App = () => {
+  return <h1>Hello There, form my React App!</h1>
+} 
+
+ReactDOM.render(<App/>, document.getElementById('app'));
