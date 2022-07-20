2ad797b22d5ad942f33abde7f533fba1323bf259
Pamela Gilmour <pgilmour@kqed.org>
2022-07-20 15:31:41 -0700

    feat(server): Adjust get method to retrieve public version of the React project

--- server/index.js
+++ server/index.js
@@ -2,10 +2,14 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require('path')
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  // res.send('Hello World!')
+  res.sendFile(path.join(__dirname), '../dist/index.html')
 })
+
+app.use(express.static(path.join(__dirname, '../dist')))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
