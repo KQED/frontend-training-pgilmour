7ca2e6226093884df23f6f477d993551c08199c7
Pamela Gilmour <pgilmour@kqed.org>
2022-07-20 11:58:23 -0700

    feat(app): Remove all code in webpack file to start fresh

--- webpack.react.config.js
+++ webpack.react.config.js
@@ -2,18 +2,5 @@
     ./webpack.config.js
 */
 const path = require('path');
-module.exports = {
-  mode: 'development',
-  entry: './react-app/index.js',
-  output: {
-    path: path.resolve('dist'),
-    filename: 'frontend_index_bundle.js'
-  },
-  module: {
-    loaders: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
-    ]
-  },
-  plugins: [HtmlWebpackPluginConfig]
-}
+
+module.exports = {}
