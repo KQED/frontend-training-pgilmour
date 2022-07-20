9ca75e8eca43c65665457ac7751bbae0b4ede6d5
Pamela Gilmour <pgilmour@kqed.org>
2022-07-20 12:32:57 -0700

    feat(app): Organize React files and components

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Greet } from './components/Header';
 
 
 console.log('Hello from my React app')
 
 const App = () => {
-  return <h1>Hello There, form my React App!</h1>
+  return() => {
+    <h1>Hello There, from my React App!</h1>;
+    <Greet />;
+
+  }
+
 } 
 
 ReactDOM.render(<App/>, document.getElementById('app'));
