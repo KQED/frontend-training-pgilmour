764608e13c557b30bec76bc379ad02ca7a7f6251
Pamela Gilmour <pgilmour@kqed.org>
2022-07-20 15:24:23 -0700

    Add webpack configuration

--- webpack.react.config.js
+++ webpack.react.config.js
@@ -1,6 +1,43 @@
-/*
-    ./webpack.config.js
-*/
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 
-module.exports = {}
+module.exports={
+
+    mode: "development", 
+
+    entry: "./react-app/src/index.js", 
+    output: {
+
+        path: path.resolve(__dirname, "dist"),
+
+        filename: "main_react_bundle.js"
+    },
+    resolve: {
+
+        extensions: ['*','.js','.jsx','.json'] 
+    },
+    module: {
+        rules: [
+          {
+            test: /\.(js|jsx)$/,
+            exclude: /(node_modules)/,
+            use: {
+              loader: "babel-loader",
+              options: {
+                presets: [
+                  "@babel/preset-env",
+                  ["@babel/preset-react", {"runtime": "automatic"}]
+                ],
+                plugins: ["@babel/plugin-transform-runtime"],
+              },
+            }
+          },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+          template: "./react-app/src/index.html",
+          publicPath: "/",
+        }),
+      ],
+}
