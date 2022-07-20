7e74bfb8d16979180ad5cd2506992d8f3f56085a
Pamela Gilmour <pgilmour@kqed.org>
2022-07-20 15:26:11 -0700

    Simplify render root

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,17 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { Greet } from './components/Header';
 
 
 console.log('Hello from my React app')
 
-const App = () => {
-  return() => {
-    <h1>Hello There, from my React App!</h1>;
-    <Greet />;
-
-  }
-
-} 
-
-ReactDOM.render(<App/>, document.getElementById('app'));
+ReactDOM.render(<Greet/>, document.getElementById('root'));
