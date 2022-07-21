72acf4347a6bade21dcca954408d7f3470f1f058
Pamela Gilmour <pgilmour@kqed.org>
2022-07-20 17:10:05 -0700

    fix(app): Update ReactDOM.render(..) to React 18 syntax

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from "react-dom/client";
 import { Greet } from './components/Header';
 
 
 console.log('Hello from my React app')
 
-ReactDOM.render(<Greet/>, document.getElementById('root'));
+// ReactDOM.render(<Greet/>, document.getElementById('root'));
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(<Greet />);
+
+
+// =================================================================
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+
+// import App from "./App";
+
+// const rootElement = document.getElementById("root");
+// const root = createRoot(rootElement);
+
+// root.render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>
+// );
