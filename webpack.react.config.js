0321c0fe6a0a3bfd474371ead7b720b43cff6715
Pamela Gilmour <pgilmour@kqed.org>
2022-08-10 09:38:34 -0700

    chore(eslint): Run eslint on code base, make corrections, debug eslint errors

--- webpack.react.config.js
+++ webpack.react.config.js
@@ -1,58 +1,52 @@
 // import path from 'path'
-const path = require("path")
+const path = require('path')
 // import HtmlWebpackPlugin from 'html-webpack-plugin' 
-const HtmlWebpackPlugin = require("html-webpack-plugin")
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 // import ESLintPlugin from 'eslint-webpack-plugin'
 const ESLintPlugin = require('eslint-webpack-plugin')
 
 
 module.exports={
-
-    mode: "development", 
-
-    entry: "./react-app/src/index.js", 
-    output: {
-
-        path: path.resolve(__dirname, "dist"),
-
-        filename: "main_react_bundle.js"
-    },
-    resolve: {
-
-        extensions: ['*','.js','.jsx','.json'] 
-    },
-
-    module: {
-        rules: [
-          {
-            test: /\.(js|jsx)$/,
-            exclude: /(node_modules)/,
-            use: {
-              loader: "babel-loader",
-              options: {
-                presets: [
-                  "@babel/preset-env",
-                  ["@babel/preset-react", {"runtime": "automatic"}]
-                ],
-                plugins: ["@babel/plugin-transform-runtime"],
-              },
-            }
+  mode: 'development',
+  entry: './react-app/src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main_react_bundle.js'
+  },
+  resolve: {
+    extensions: ['*','.js','.jsx','.json'] 
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              ['@babel/preset-react', { 'runtime': 'automatic' }]
+            ],
+            plugins: ['@babel/plugin-transform-runtime'],
           },
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-          template: "./react-app/src/index.html",
-          publicPath: "/",
-        }),
-        new ESLintPlugin(
-          // {
-          // extensions:['js', 'jsx', 'json']
-        // }
-        ),
-      ],
-      watch: true,
-      watchOptions: {
-        ignored: 'path.resolve(__dirname, node_modules)',
+        }
       },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './react-app/src/index.html',
+      publicPath: '/',
+    }),
+    new ESLintPlugin(
+      // {
+      // extensions:['js', 'jsx', 'json']
+      // }
+    ),
+  ],
+  watch: true,
+  watchOptions: {
+    ignored: 'path.resolve(__dirname, node_modules)',
+  }
 }
