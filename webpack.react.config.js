044aa56440ea375dc05c0d123d12764660d44247
Pamela Gilmour <pgilmour@kqed.org>
2022-08-10 15:35:38 -0700

    Adjust public path

--- webpack.react.config.js
+++ webpack.react.config.js
@@ -41,7 +41,7 @@ module.exports={
   plugins: [
     new HtmlWebpackPlugin({
       template: './react-app/index.html',
-      publicPath: '/index.html',
+      publicPath: '/',
     }),
     new ESLintPlugin(
       {
