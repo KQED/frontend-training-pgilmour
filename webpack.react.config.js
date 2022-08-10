b5201b61ad2734b26d637d24995869c7d140ab75
Pamela Gilmour <pgilmour@kqed.org>
2022-08-10 15:26:13 -0700

    Fix linting errors

--- webpack.react.config.js
+++ webpack.react.config.js
@@ -1,43 +1,38 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin")
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-module.exports={
-
-    mode: "development", 
-
-    entry: "./react-app/src/index.js", 
-    output: {
-
-        path: path.resolve(__dirname, "dist"),
-
-        filename: "main_react_bundle.js"
-    },
-    resolve: {
-
-        extensions: ['*','.js','.jsx','.json'] 
-    },
-    module: {
-        rules: [
-          {
-            test: /\.(js|jsx)$/,
-            exclude: /(node_modules)/,
-            use: {
-              loader: "babel-loader",
-              options: {
-                presets: [
-                  "@babel/preset-env",
-                  ["@babel/preset-react", {"runtime": "automatic"}]
-                ],
-                plugins: ["@babel/plugin-transform-runtime"],
-              },
-            }
+module.exports = {
+  mode: 'development',
+  entry: './react-app/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main_react_bundle.js'
+  },
+  resolve: {
+    extensions: ['*','.js','.jsx','.json', '.wasm']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              ['@babel/preset-react', { 'runtime': 'automatic' }]
+            ],
+            plugins: ['@babel/plugin-transform-runtime'],
           },
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-          template: "./react-app/src/index.html",
-          publicPath: "/",
-        }),
-      ],
+        }
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './react-app/index.html',
+      publicPath: '/',
+    }),
+  ]
 }
