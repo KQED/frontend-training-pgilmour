accfe558cf040f3a0d0a95b78c6797bd17e75883
Pamela Gilmour <pgilmour@kqed.org>
2022-09-09 12:55:37 -0700

    chore(root): Install Redux and related packages

--- react-app/components/Header.jsx
+++ react-app/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export class Greet extends React.Component {
+export class App extends React.Component {
   render () {
     return <h2>Hello there ugly, I&lsquo;m a header component!</h2>
   }
--- react-app/index.jsx
+++ react-app/index.jsx
@@ -1,8 +1,21 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { Greet } from './components/Header'
+
+// import { Provider } from 'react-redux'
+// import store from './store'
+
+import { App } from './components/Header'
 
 const rootElement = document.getElementById('root')
 const root = createRoot(rootElement)
 
-root.render(<Greet />)
+root.render(<App />)
+
+// const root = ReactDOM.createRoot(document.getElementById('root'))
+// root.render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>
+// )
