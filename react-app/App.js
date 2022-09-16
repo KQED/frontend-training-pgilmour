fea625df74c82e207b227953889be6894309144b
Pamela Gilmour <pgilmour@kqed.org>
2022-09-16 12:16:07 -0700

    Add a root App.js file instead of using Header.js as the root of my project, and import Header component

---
+++ react-app/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Header } from './components/Header' // moving this to ./App.js
+
+function App () {
+  return (
+    <div className='App'>
+      <Header />
+    </div>
+  )
+}
+  
+export default App
