d8be6e31968dd37e9a68c6d6f8c3b7d8bb9c5923
Pamela Gilmour <pgilmour@kqed.org>
2022-09-16 12:18:55 -0700

    Whitespace and comment cleanup, added some commented code for some Redux prep

--- react-app/index.jsx
+++ react-app/index.jsx
@@ -1,21 +1,27 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
 import React from 'react'
+// import ReactDOM from 'react-d÷om/client' // No longer needed, using an updated protocol 
 import { createRoot } from 'react-dom/client'
-
+import App from './App'
+// import {  } from './components/Header' // moving this to ./App.js
 // import { Provider } from 'react-redux'
 // import store from './store'
-
-import { App } from './components/Header'
 
 const rootElement = document.getElementById('root')
 const root = createRoot(rootElement)
 
-root.render(<App />)
+root.render(<App />) // Probably add Provider Component here.
 
+// These lines are prep for Redux ticket
 // const root = ReactDOM.createRoot(document.getElementById('root'))
 // root.render(
 //   <Provider store={store}>
 //     <App />
 //   </Provider>
+// )
+
+// const root = ReactDOM.createRoot(document.getElementById('root'))
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
 // )
