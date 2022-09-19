3537443dc5aea39d20dcce99dabe34e792c6e896
Pamela Gilmour <pgilmour@kqed.org>
2022-09-19 16:32:56 -0700

    feat(HexPicker): Bring in useSelector, useDispatch, and randomize reducer, implement accordingly

--- react-app/src/features/hexPicker/HexPicker.js
+++ react-app/src/features/hexPicker/HexPicker.js
@@ -1,24 +1,24 @@
 // Create a subcomponent in your application
 // a React component that shows the UI for your feature
 import React from 'react'
-// import { useSelector, useDispatch } from 'react-redux'
-// import { randomize } from './hexPickerSlice'
+import { useSelector, useDispatch } from 'react-redux'
+import { randomize } from './hexPickerSlice'
 // import styles from './Counter.module.css'
 
 export function HexPicker () {
-  // const generatedValue = useSelector((state) => state.hexPicker.valueRGB)
-  // const dispatch = useDispatch()
+  const generatedValue = useSelector((state) => state.hexPicker.valueRGB)
+  const dispatch = useDispatch()
 
   return (
     <div>
       <div>
         <button
           aria-label='Generate random RGB value'
-          // onClick={() => dispatch(randomize())}
+          onClick={() => dispatch(randomize())}
         >
           Click to Generate a Random RBG Value
         </button>
-        {/* <span>{generatedValue}</span> */}
+        <span>{generatedValue}</span>
       </div>
     </div>
   )
