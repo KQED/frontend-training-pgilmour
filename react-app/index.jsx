5bbd7b1e39ef3f33fd8654d46806f3fa21e8d50f
Pamela Gilmour <pgilmour@kqed.org>
2022-09-19 12:31:25 -0700

    feat(root): Add Redux Provider component to the root and other Redux related code for setup

--- react-app/index.jsx
+++ react-app/index.jsx
@@ -1,27 +1,15 @@
 import React from 'react'
-// import ReactDOM from 'react-d÷om/client' // No longer needed, using an updated protocol 
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
 import App from './App'
-// import {  } from './components/Header' // moving this to ./App.js
-// import { Provider } from 'react-redux'
-// import store from './store'
+import createStore from './createReduxStore'
 
+const store = createStore()
 const rootElement = document.getElementById('root')
 const root = createRoot(rootElement)
 
-root.render(<App />) // Probably add Provider Component here.
-
-// These lines are prep for Redux ticket
-// const root = ReactDOM.createRoot(document.getElementById('root'))
-// root.render(
-//   <Provider store={store}>
-//     <App />
-//   </Provider>
-// )
-
-// const root = ReactDOM.createRoot(document.getElementById('root'))
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// )
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
