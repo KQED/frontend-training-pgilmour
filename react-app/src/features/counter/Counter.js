b8f3f2a389cab745c475f2be7ff01d3905aa6b21
Pamela Gilmour <pgilmour@kqed.org>
2022-09-19 14:16:09 -0700

    chore(Redux store): Add sample starter component code

--- react-app/src/features/counter/Counter.js
+++ react-app/src/features/counter/Counter.js
@@ -1,1 +1,30 @@
 // a React component that shows the UI for the counter feature
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from './counterSlice'
+// import styles from './Counter.module.css'
+
+export function Counter () {
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
+
+  return (
+    <div>
+      <div>
+        <button
+          aria-label='Increment value'
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label='Decrement value'
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
+  )
+}
