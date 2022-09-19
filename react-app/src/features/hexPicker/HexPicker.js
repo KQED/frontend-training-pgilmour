a4de6b334f33e57c05cc8340781eb2178730d4d8
Pamela Gilmour <pgilmour@kqed.org>
2022-09-19 16:02:52 -0700

    feat(HexPicker): Clean up comments and adjust starter code to our purposes of generating a random rgb value

--- react-app/src/features/hexPicker/HexPicker.js
+++ react-app/src/features/hexPicker/HexPicker.js
@@ -1,39 +1,25 @@
 // Create a subcomponent in your application
-
 // a React component that shows the UI for your feature
 import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { decrement, increment } from './counterSlice'
+// import { useSelector, useDispatch } from 'react-redux'
+// import { randomize } from './hexPickerSlice'
 // import styles from './Counter.module.css'
 
 export function HexPicker () {
-  // const count = useSelector((state) => state.counter.value)
+  // const generatedValue = useSelector((state) => state.hexPicker.valueRGB)
   // const dispatch = useDispatch()
 
   return (
     <div>
       <div>
-          {/* The component should display some "data" */}
-         {/*  The component should have some sort of "button" */}
-         <button
-           aria-label='Increment value'
-           onClick={() => dispatch(increment())}
-           When you click the button it should fire off the "dispatch"
-          //  The dispatch should have some sort of data as a payload
-         >
-           Increment
-         </button>
-         {/*  The component should display some "data" */}
-         {/*  The store should update   This would be in the other realted file , like counterSlice.js*/}
-         {/*  The component should display the new data from the store */}
-         <span>{count}</span> 
-         <button
-           aria-label='Decrement value'
-           onClick={() => dispatch(decrement())}
-         >
-           Decrement
-         </button>
-       </div>
+        <button
+          aria-label='Generate random RGB value'
+          // onClick={() => dispatch(randomize())}
+        >
+          Click to Generate a Random RBG Value
+        </button>
+        {/* <span>{generatedValue}</span> */}
+      </div>
     </div>
   )
 }
