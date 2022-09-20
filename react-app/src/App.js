0161f671595a242bcd8c1cce23955ca889aee5d6
Pamela Gilmour <pgilmour@kqed.org>
2022-09-20 12:28:18 -0700

    feat(KSM-3475, App.js): Remove starter component

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import { Header } from './components/Header'
 import Body from './components/Body'
-import { Counter } from './features/counter/Counter'
 
 function App () {
   return (
     <div className='App'>
       <Header />
       <Body />
-      <Counter />
     </div>
   )
 }
