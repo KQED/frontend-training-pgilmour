028f30be3a2e549e7c43aaccb601bd446c399bf4
Pamela Gilmour <pgilmour@kqed.org>
2022-09-20 15:48:00 -0700

    feat(KSM-3475, HexPicker): Add a function to change the color of the page according to the generated value from the state slice, rearrange button and p tags

--- react-app/src/features/hexPicker/HexPicker.js
+++ react-app/src/features/hexPicker/HexPicker.js
@@ -1,24 +1,25 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { randomize } from './hexPickerSlice'
-import style from './HexPicker.scss'
 
 export function HexPicker () {
   const generatedValue = useSelector((state) => state.hexPicker.valueRGB)
   const dispatch = useDispatch()
-
+  const applyColor = () => {
+    document.body.style.background = generatedValue
+  }
+  applyColor()
   return (
     <div>
       <h2>Random RGB Generator</h2>
       <section>
+        <p>{generatedValue}</p>
         <button
           aria-label='Generate random RGB value'
           onClick={() => dispatch(randomize())}
         >
           Click to Generate a Random RBG Value
         </button>
-        <p>{generatedValue}</p>
-        <div className={style} id='swatch'></div>
       </section>
     </div>
   )
