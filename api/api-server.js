b666cf08e1c1de8d823281062e0ca0f85716ef19
Pamela Gilmour <pgilmour@kqed.org>
2022-10-24 14:12:05 -0700

    feat(KSM-3687, api-server.js): - Add dummy data to this file instead as a external file - Created a more useful greeting for my root route - Created a route that returns all the dummy data - Created a route that returns one user by 'id' - Created a route that takes JSON data and sets a flag named 'loggedin' as true

--- api/api-server.js
+++ api/api-server.js
@@ -1,26 +1,36 @@
-// Starter code from https://expressjs.com/en/starter/hello-world.html
+// https://github.com/expressjs/express/blob/master/examples/params/index.js
 const express = require('express')
 const app = express()
 const port = 3001
-// const path = require('path')
+
+app.use(express.json())
+
+let users = [
+  { id: 33, name: 'Sailor Moon', alias: 'Usagi' },
+  { id: 34, name: 'Sailor Mercury', alias: 'Amy' },
+  { id: 98, firstName: 'Reiko', alias: 'Sailor Aluminum Siren' }
+]
 
 app.get('/', (req, res) => {
-  return res.send('Hello from API Server.')
+  res.send('Try routes `~/users` or `~/get-user/34`')
 })
 
-// app.get('/', (req, res) => {
-//   return res.send('Recieved a GET HTTP method')
-// })
+app.get('/users', (req, res) => {
+  res.send(users)
+})
 
-// app.post('/', (req, res) => {
-//   return res.send('Recieved a POST HTTP method')
-// })
+app.get('/get-user/:id', (req, res) => {
+  let user = users.find(element => element.id === Number(req.params.id))
+  res.send(user)
+})
 
-// app.oyt('/', (req, res) => {
-//   return res.send('Recieved a PUT HTTP method')
-// })
 
-// app.use(express.static(path.join(__dirname, '../dist')))
+// This endpoint takes an existing 'id' and sets a flag called 'loggedin' as true.
+app.post('/set-user/:id', (req, res) => {
+  let userData = req.body
+  userData.loggedin = true // This is the flag requested in the ticket
+  res.json({ requestBody: req.body })
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
