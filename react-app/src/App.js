c462dfd86a6649b95ad614a6cd2eea6da662e7f2
Pamela Gilmour <pgilmour@kqed.org>
2022-11-23 11:06:12 -0800

    feat(KSM-3865, App.js): Add Form component to App

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,12 +1,15 @@
+// import React, { useEffect } from 'react'
 import React from 'react'
-import { Header } from './components/Header'
-import Body from './components/Body'
+import Header from './components/Header/Header'
+import Body from './components/Body/Body'
+import Form from './components/Form/Form'
 
 function App () {
   return (
     <div className='App'>
       <Header />
       <Body />
+      <Form />
     </div>
   )
 }
