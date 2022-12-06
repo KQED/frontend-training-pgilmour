c85eb1a2d98bb52793d302b358a24a5fcae3c5d5
Pamela Gilmour <pgilmour@kqed.org>
2022-12-06 15:48:35 -0800

    feat(KSM-3865, Form/): Move Form component into features directory because it will be used with Redux

--- react-app/src/features/Form/Form.jsx
+++ react-app/src/features/Form/Form.jsx
@@ -1,26 +1,22 @@
 import React from 'react'
-import styles from './Form.scss'
 // https://reactjs.org/docs/forms.html
-// Use a form on the front end to input, modify, and display the data
-// import cx from 'classnames'
-
 
 function Form () {
   console.log('Hello')
   return (
-    <div className={styles}>
+    <div>
       <div>
-        <h2 id='addData'>Add a Sailor Scout:</h2>
+        <h2 id='getData'>Get a Sailor Scout:</h2>
         <form>
           <label>Sailor&apos;s Name:
             <input type='text' id='addName' name='addName' placeholder='Sailor Moon' /><br/>
           </label><br/>
-          <label>Sailor&apos;s Alias:
+          {/* <label>Sailor&apos;s Alias:
             <input type='text' id='addAlias' name='addAlias' placeholder='Usagi'/><br/>
-          </label><br/>
-          <label>Sailor&apos;s Alignment:
+          </label><br/> */}
+          {/* <label>Sailor&apos;s Alignment:
             <input type='text' id='addAlignment' name='addAlignment' placeholder='Moon Kingdom'/><br/>
-          </label><br/>
+          </label><br/> */}
           <input type='submit' value='Submit' />
         </form>
       </div>
