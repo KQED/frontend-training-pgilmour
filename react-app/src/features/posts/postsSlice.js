6adee45ada85a9033625cd1aa007702dd3b90589
Pamela Gilmour <pgilmour@kqed.org>
2022-12-05 18:06:16 -0800

    feat(KSM-3865, postsSlice.js): Add slice of state

--- react-app/src/features/posts/postsSlice.js
+++ react-app/src/features/posts/postsSlice.js
@@ -1,14 +1,38 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 
-const initialState = [
-  { id: '1', title: 'First Post!', content: 'Hello!' },
-  { id: '2', title: 'Second Post', content: 'More text' }
-]
+export const getPosts = createAsyncThunk('posts/getPosts', async () => {
+  // fetch doesnt work in this instance
+  // need to look at createAsyncThunk to put this fetch call into a fx or something like that
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+  console.log('response', response)
+  return response.data
+})
+
+const initialState = {
+  loading: false,
+  posts: [],
+  error: ''
+}
 
 export const postSlice = createSlice(({
   name: 'posts',
   initialState,
-  reducers: {}
+  reducers: {
+    pending: (state) => {
+      state.loading = true
+    },
+    fulfilled: (state, action) => {
+      state.loading = false
+      state.posts = action.payload
+      state.error = ''
+    },
+    rejected: (state, action) => {
+      state.loading = false
+      state.posts = []
+      state.error = action.error.message
+    }
+  }
 }))
 
+export const { pending, fulfilled, rejected } = postSlice.actions
 export default postSlice.reducer
