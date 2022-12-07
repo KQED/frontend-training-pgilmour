c6b3d18a5e51109ae03cd3be5d4bd6f8baabc851
Pamela Gilmour <pgilmour@kqed.org>
2022-12-07 13:02:54 -0800

    feat(KSM-3865, store.js): Clean up comments and comment in sailorScoutApi related code

--- react-app/src/app/store.js
+++ react-app/src/app/store.js
@@ -1,25 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit'
-// import counterReducer from '../features/counter/counterSlice'
 import hexcodeReducer from '../features/hexPicker/hexPickerSlice'
 import postsReducer from '../features/posts/postsSlice'
 import { setupListeners } from '@reduxjs/toolkit/query'
-import { pokemonApi } from '../services/pokemon'
-// import { sailorScoutApi } from '../services/sailorScout'
+import { sailorScoutApi } from '../services/sailorScout'
 
 export const store = configureStore({
   reducer: {
     // Add the generated reducer as a specific top-level slice
-    [pokemonApi.reducerPath]: pokemonApi.reducer,
-    // [sailorScoutApi.reducerPath]: sailorScoutApi.reducer,
-    // counter: counterReducer,
+    [sailorScoutApi.reducerPath]: sailorScoutApi.reducer,
     hexPicker: hexcodeReducer,
     posts: postsReducer
   },
   // Adding the api middleware enables caching, invalidation, polling,
   // and other useful features of `rtk-query`.
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(pokemonApi.middleware)
-  //   getDefaultMiddleware().concat(sailorScoutApi.middleware)
+    getDefaultMiddleware().concat(sailorScoutApi.middleware)
 })
 
 // optional, but required for refetchOnFocus/refetchOnReconnect behaviors
