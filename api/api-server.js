874f0d21964ed60d07e467e2ee9d949c8c95940e
Pamela Gilmour <pgilmour@kqed.org>
2023-01-03 14:06:44 -0800

    chore(ksm-3865, api/api-server.js, react-app/index.html, server/index.js): Give apps more informative names

--- api/api-server.js
+++ api/api-server.js
@@ -33,5 +33,5 @@ app.post('/set-user/:id', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Api app listening on port ${port}`)
 })
