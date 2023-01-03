ab390ec77dcb165073778fa0070dab2997f60cc5
Pamela Gilmour <pgilmour@kqed.org>
2023-01-03 13:04:04 -0800

    Comment clean up, simplify the form

--- react-app/src/features/Form/Form.jsx
+++ react-app/src/features/Form/Form.jsx
@@ -1,45 +1,38 @@
 import React from 'react'
+// https://redux.js.org/style-guide/
+// Redux recommends avoiding putting form state in Redux
+// prefer keeping the form edits themselves in local component state,
+// and only dispatching an action to update the Redux store 
+// once the user has completed the form.
 // https://reactjs.org/docs/forms.html
+import { useGetScoutByAliasQuery } from '../../services/sailorScout.js'
 
-function Form () {
-  console.log('Hello')
+
+export default function Form () {
+  console.log('Hello from From.jsx')
+  const { data, error, isLoading } = useGetScoutByAliasQuery('Amy')
+  console.log('From Form.jsx: ', data)
   return (
     <div>
       <div>
         <h2 id='getData'>Get a Sailor Scout:</h2>
         <form>
-          <label>Sailor&apos;s Name:
-            <input type='text' id='addName' name='addName' placeholder='Sailor Moon' /><br/>
-          </label><br/>
-          {/* <label>Sailor&apos;s Alias:
-            <input type='text' id='addAlias' name='addAlias' placeholder='Usagi'/><br/>
-          </label><br/> */}
-          {/* <label>Sailor&apos;s Alignment:
-            <input type='text' id='addAlignment' name='addAlignment' placeholder='Moon Kingdom'/><br/>
-          </label><br/> */}
-          <input type='submit' value='Submit' />
-        </form>
-      </div>
-      <div>
-        <h2 id='modifyData'>Change a Sailor Scout&apos;s entry:</h2>
-        <form>
-          <label>Sailor&apos;s Name:
-            <input type='text' id='changeName' name='changeName' placeholder='Sailor Moon' /><br/>
-          </label><br/>
           <label>Sailor&apos;s Alias:
-            <input type='text' id='changeAlias' name='changeAlias' placeholder='Usagi'/><br/>
-          </label><br/>
-          <label>Sailor&apos;s Alignment:
-            <input type='text' id='changeAlignment' name='changeAlignment' placeholder='Moon Kingdom'/><br/>
+            <input type='text' id='getName' name='getName' placeholder='Usagi' /><br/>
           </label><br/>
           <input type='submit' value='Submit' />
         </form>
       </div>
       <div>
         <h2 id='displayInput'>Your Sailor entry:</h2>
+        {error ? (<>Oh no, there was an error</>) : isLoading ? (<>Loading...</>) : data ? (
+          <>
+            <h3>{data.alias}</h3>
+            <p>{data.name}</p>
+            <p>{data.alignment}</p>
+          </>
+        ) : null}
       </div>
     </div>
   )
 }
-
-export default Form
