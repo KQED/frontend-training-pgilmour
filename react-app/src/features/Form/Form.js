b005c750da2c35354aa99c94525f923bca40cd1a
Pamela Gilmour <pgilmour@kqed.org>
2023-01-23 14:57:37 -0800

    Add links to docs, create state variables, add two basic event handlers

--- react-app/src/features/Form/Form.js
+++ react-app/src/features/Form/Form.js
@@ -1,14 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styles from './Form.scss'
+// https://medium.com/swlh/building-controlled-forms-using-functional-components-in-react-965d033a89bd
 
-export function Form () {
+const Form = () => {
+  // To Do 1.18.23: Review the links below to figure out your form
+  // https://blog.isquaredsoftware.com/presentations/react-redux-ts-intro-2020-12/#/46
+  // https://blog.isquaredsoftware.com/presentations/react-redux-ts-intro-2020-12/#/47
+  // https://blog.isquaredsoftware.com/presentations/react-redux-ts-intro-2020-12/#/82
+  // https://blog.isquaredsoftware.com/presentations/react-redux-ts-intro-2020-12/#/86
+  // https://redux.js.org/tutorials/essentials/part-5-async-logic
+  
+  const [name, setName] = useState('')
+
+  const handleChange = event => {
+    setName(event.target.value)
+  }
+  const handleSubmit = event => {
+    event.preventDefault()
+    // submit the form
+  }
   return (
-    <div>
-      <h2>Form goes here:</h2>
-      {/* "Use a form on the front end to input, modify, and display the data" */}
-      <form>
-        <label> Enter a Sailor Scout&apos;s name:
-          <input type='text' placeholder='Sailor Moon' />
-        </label>
+    <div className={styles}>
+      <p className={styles}>Your input was:</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Enter a Sailor Scout&apos;s Name (Sailor Moon): <br /><br />
+          <input type='text' value={name} onChange={handleChange} />
+        </label><br />
+        <input type='submit' value='Submit' />
       </form>
     </div>
   )
