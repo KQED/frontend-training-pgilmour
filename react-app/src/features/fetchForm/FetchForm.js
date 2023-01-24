80a0c87c262fe96ad57914865024d42bdda83704
Pamela Gilmour <pgilmour@kqed.org>
2023-01-24 11:03:54 -0800

    chore(ksm-3865, FetchForm.js): Map users

--- react-app/src/features/fetchForm/FetchForm.js
+++ react-app/src/features/fetchForm/FetchForm.js
@@ -1,27 +1,20 @@
 import React from 'react'
-// import { useSelector, useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
 // import { randomize } from '../../slices/hexPickerSlice'
 // import style from './HexPicker.scss'
 
-export function FetchForm () {
-  // const generatedValue = useSelector((state) => state.hexPicker.valueRGB)
-  // const dispatch = useDispatch()
-  // const applyColor = () => {
-  //   document.body.style.background = generatedValue
-  // }
-  // applyColor()
+export const FetchForm = () => {
+  const users = useSelector(state => state.users)
+
+  const renderedUsers = users.map(user => (
+    <div key={user.id}>
+      <h3>{user.name}</h3>
+    </div>
+  ))
   return (
     <div>
-      {/*
-      <section>
-        <p>{generatedValue}</p>
-        <button
-          aria-label='Generate random RGB value'
-          onClick={() => dispatch(randomize())}
-        >
-          Click to Generate a Random RBG Value
-        </button>
-      </section> */}
+      <h2>JSON response:</h2>
+      {renderedUsers}
       <p>Hello FetchForm.js</p>
       <p>Results from Redux fetch for all users: </p>
     </div>
