58f96da5e9b26555af6a9aaa16598ef15cbcc7fd
Pamela Gilmour <pgilmour@kqed.org>
2023-01-24 11:04:17 -0800

    chore(ksm-3865, store.js): Add fetch reducer

--- react-app/src/app/store.js
+++ react-app/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import hexcodeReducer from '../slices/hexPickerSlice'
+import fetchFormReducer from '../features/fetchForm/FetchForm'
 
 export default configureStore({
   reducer: {
-    hexPicker: hexcodeReducer
+    hexPicker: hexcodeReducer,
+    fetchForm: fetchFormReducer
   }
 })
