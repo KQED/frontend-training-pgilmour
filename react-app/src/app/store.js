0955d92dd70ab4bf816e6cdf6a17f8b261e33e1c
Pamela Gilmour <pgilmour@kqed.org>
2023-01-25 11:20:13 -0800

    chores(ksm-3865, store.js): - Reinstate sailorScoutApi related code
    - Remove commented notes on an alternative way of writing this code
    - Add sailorScoutApi.middleware to middleware key

--- react-app/src/app/store.js
+++ react-app/src/app/store.js
@@ -2,47 +2,21 @@ import { configureStore } from '@reduxjs/toolkit'
 import hexcodeReducer from '../slices/hexPickerSlice'
 import { setupListeners } from '@reduxjs/toolkit/query'
 import { pokemonApi } from '../services/pokemon'
-// import { sailorScoutApi } from '../services/sailorScout'
+import { sailorScoutApi } from '../services/sailorScout'
 
 export const store = configureStore({
   reducer: {
     // Add the generated reducer as a specific top-level slice
     [pokemonApi.reducerPath]: pokemonApi.reducer,
-    // [sailorScoutApi.reducerPath]: sailorScoutApi.reducer,
+    [sailorScoutApi.reducerPath]: sailorScoutApi.reducer,
     hexPicker: hexcodeReducer
   },
   // Adding the api middleware enables caching, invalidation, polling,
   // and other useful features of `rtk-query`.
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(pokemonApi.middleware)
-    // getDefaultMiddleware().concat(sailorScoutApi.middleware)
+    getDefaultMiddleware().concat([pokemonApi.middleware, sailorScoutApi.middleware])
 })
 
 // optional, but required for refetchOnFocus/refetchOnReconnect behaviors
 // see `setupListeners` docs - takes an optional callback as the 2nd arg for customization
 setupListeners(store.dispatch)
-
-
-// The following is a different way of doing all of the code above:
-
-// import { combineReducers, configureStore } from '@reduxjs/toolkit'
-// import type { PreloadedState } from '@reduxjs/toolkit'
-// import { pokemonApi } from './services/pokemon'
-
-// const rootReducer = combineReducers({
-//   [pokemonApi.reducerPath]: pokemonApi.reducer,
-// })
-
-// export const setupStore = (preloadedState?: PreloadedState<RootState>) => {
-//   return configureStore({
-//     reducer: rootReducer,
-//     middleware: (getDefaultMiddleware) =>
-//       // adding the api middleware enables caching, invalidation, polling and other features of `rtk-query`
-//       getDefaultMiddleware().concat(pokemonApi.middleware),
-//     preloadedState,
-//   })
-// }
-
-// export type RootState = ReturnType<typeof rootReducer>
-// export type AppStore = ReturnType<typeof setupStore>
-// export type AppDispatch = AppStore['dispatch']
