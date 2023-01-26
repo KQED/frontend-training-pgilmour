d3d2f27786a19b5ebd05767822ed360c4d56a41a
Pamela Gilmour <pgilmour@kqed.org>
2023-01-26 12:43:33 -0800

    chore(ksm-3865, Body.jsx): Add new Form component into the Body component

--- react-app/src/components/Body/Body.jsx
+++ react-app/src/components/Body/Body.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { HexPicker } from '../../features/hexPicker/HexPicker'
+import Form from '../../features/Form/Form'
 
 function Body () {
   return (
     <div>
       <HexPicker />
+      <Form />
     </div>
   )
 }
