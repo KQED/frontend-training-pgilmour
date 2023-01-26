a7d082de2d849558d29c95ac9b2f161f94d0da34
Pamela Gilmour <pgilmour@kqed.org>
2023-01-26 12:52:56 -0800

    chore(ksm-3865, RenderPokemon.js): Give component a name and add commented notes from RTKQ docs

--- react-app/src/features/PokemonExample/RenderPokemon.js
+++ react-app/src/features/PokemonExample/RenderPokemon.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useGetPokemonByNameQuery } from './services/pokemon'
 
-function App () {
+function RenderPokemon () {
   // Using a query hook automatically fetches data and returns query values
   const { data, error, isLoading } = useGetPokemonByNameQuery('bulbasaur')
   // Individual hooks are also accessible under the generated endpoints:
@@ -23,4 +23,33 @@ function App () {
   )
 }
 
-export default App
+export default RenderPokemon
+
+// Another way to the do the code above:
+
+// import './styles.css'
+// import { Pokemon } from './Pokemon' // I have included this code in the code base
+// import { useState } from 'react'
+
+// const pokemon = ['bulbasaur', 'pikachu', 'ditto', 'bulbasaur']
+
+// export default function App() {
+//   const [pollingInterval, setPollingInterval] = useState(0)
+
+//   return (
+//     <div className="App">
+//       <select
+//         onChange={(change) => setPollingInterval(Number(change.target.value))}
+//       >
+//         <option value={0}>Off</option>
+//         <option value={1000}>1s</option>
+//         <option value={5000}>5s</option>
+//       </select>
+//       <div>
+//         {pokemon.map((poke, index) => (
+//           <Pokemon key={index} name={poke} pollingInterval={pollingInterval} />
+//         ))}
+//       </div>
+//     </div>
+//   )
+// }
