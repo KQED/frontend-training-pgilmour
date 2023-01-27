1e3929210b1bd1c008a3e8ec5db06e0a5f350774
Pamela Gilmour <pgilmour@kqed.org>
2023-01-26 17:12:54 -0800

    chore(ksm-3865, Form.jsx): Remove unused comments, add to do, set up form as a simple react form based on React docs, comment out Redux code for now

--- react-app/src/features/Form/Form.jsx
+++ react-app/src/features/Form/Form.jsx
@@ -1,27 +1,38 @@
-import React from 'react'
-// https://redux.js.org/style-guide/
-// Redux recommends avoiding putting form state in Redux
-// prefer keeping the form edits themselves in local component state,
-// and only dispatching an action to update the Redux store 
-// once the user has completed the form.
-// https://reactjs.org/docs/forms.html
-import { useGetScoutByAliasQuery } from '../../services/sailorScout.js'
+import React, { useState } from 'react'
+// import { useGetScoutByAliasQuery } from '../../services/sailorScout.js'
 
-
+{/* <li>Create a Form that takes the input to fetch from your sailor api</li> */}
 export default function Form () {
-  const { data, error, isLoading } = useGetScoutByAliasQuery('Amy')
+  const [ alias, setAlias ] = useState('')
+  // const { data, error, isLoading } = useGetScoutByAliasQuery('Reiko')
+  // const { data, error, isLoading } = useGetScoutByAliasQuery(document.getElementById('getName'))
+  const handleChange = (event) => {
+    setAlias(event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    alert('An alias was submitted: ' + alias)
+    event.preventDefault
+  }
+
   return (
     <div>
       <div>
         <h2 id='getData'>Get a Sailor Scout:</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <label>Sailor&apos;s Alias:
-            <input type='text' id='getName' name='getName' placeholder='Usagi' /><br/>
+            <input
+              type='text'
+              value={alias}
+              placeholder='Usagi'
+              onChange={handleChange}
+            />
+            <br/>
           </label><br/>
           <input type='submit' value='Submit' />
         </form>
       </div>
-      <div>
+      {/* <div>
         <h2 id='displayInput'>Your Sailor entry:</h2>
         {error ? (<>Oh no, there was an error</>) : isLoading ? (<>Loading...</>) : data ? (
           <>
@@ -30,7 +41,7 @@ export default function Form () {
             <p>{data.alignment}</p>
           </>
         ) : null}
-      </div>
+      </div> */}
     </div>
   )
 }
