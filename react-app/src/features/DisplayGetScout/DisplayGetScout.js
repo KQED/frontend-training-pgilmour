586fd7bd75ed49893532c1974b440f6ca5e30a18
Pamela Gilmour <pgilmour@kqed.org>
2023-02-07 14:40:09 -0800

    chore(KSM-3865 ) : Adjust notes

--- react-app/src/features/DisplayGetScout/DisplayGetScout.js
+++ react-app/src/features/DisplayGetScout/DisplayGetScout.js
@@ -4,7 +4,6 @@ import { useGetScoutByAliasQuery } from '../../services/sailorScout.js'
 export default function DisplayGetScout () {
   // Using a query hook automatically fetches data and returns query values
   const { data, error, isLoading } = useGetScoutByAliasQuery('Reiko')
-  // Individual hooks are also accessible under the generated endpoints:
   return (
     <div>
       {error ? (
@@ -19,13 +18,13 @@ export default function DisplayGetScout () {
           <p>{data.alignment}</p>
           <h3> Next adjust tasks:</h3>
           <ul>
-            <li> Create a form in React</li>
-            <li>Create a Form that takes the input to fetch from your sailor api</li>
+            <li>completed - Create a form in React</li>
+            <li>in progress - Create a Form that takes the input to fetch from your sailor api</li>
             <li> create a field to add a new sailor</li>
             <li> create a field to modify an existing sailor</li>
             <li> |_ then fetch the newly modified data directly from the modification mentioned above</li>
           </ul>
-          <h3> Stretch goal is get a gif fropm giphy</h3>
+          <h3> Stretch goal is get a gif from giphy</h3>
         </>
       ) : null}
     </div>
