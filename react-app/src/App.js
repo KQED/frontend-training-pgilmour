0584b0e72a75c52310800b7147e30c389af0d657
Pamela Gilmour <pgilmour@kqed.org>
2023-02-07 15:24:06 -0800

    feat(ksm-3865, Redux Setup): Remove unused components

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,16 +1,12 @@
 import React from 'react'
 import Header from './components/Header/Header'
 import Body from './components/Body/Body'
-import RenderPokemon from './features/PokemonExample/RenderPokemon'
-import DisplayGetScout from './features/DisplayGetScout/DisplayGetScout'
 
 function App () {
   return (
     <div className='App'>
       <Header />
       <Body />
-      <RenderPokemon />
-      <DisplayGetScout />
     </div>
   )
 }
