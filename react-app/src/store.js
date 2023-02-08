413e6ffd70876c589b69cd37b2f6cd5fb4e9ed1d
Pamela Gilmour <pgilmour@kqed.org>
2023-02-08 12:40:25 -0800

    feat(ksm-3865, Redux fetch): Remove unneeded code

--- react-app/src/store.js
+++ react-app/src/store.js
@@ -1,20 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit'
 import hexcodeReducer from './slices/hexPickerSlice'
 import { setupListeners } from '@reduxjs/toolkit/query'
-import { pokemonApi } from './services/pokemon'
 import { sailorScoutApi } from './services/sailorScout'
 
 export const store = configureStore({
   reducer: {
     // Add the generated reducer as a specific top-level slice
-    [pokemonApi.reducerPath]: pokemonApi.reducer,
     [sailorScoutApi.reducerPath]: sailorScoutApi.reducer,
     hexPicker: hexcodeReducer
   },
   // Adding the api middleware enables caching, invalidation, polling,
   // and other useful features of `rtk-query`.
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat([pokemonApi.middleware, sailorScoutApi.middleware])
+    getDefaultMiddleware().concat([sailorScoutApi.middleware])
 })
 
 // optional, but required for refetchOnFocus/refetchOnReconnect behaviors
