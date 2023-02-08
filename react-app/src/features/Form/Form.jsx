6b2f845cd411bec45fb19bc6faf2af791ee042e3
Pamela Gilmour <pgilmour@kqed.org>
2023-02-08 12:39:51 -0800

    feat(ksm-3865, Redux fetch): Add another form to modify data

--- react-app/src/features/Form/Form.jsx
+++ react-app/src/features/Form/Form.jsx
@@ -3,21 +3,27 @@ import { useGetScoutByAliasQuery } from '../../services/sailorScout.js'
 
 export default function Form () {
   const [ alias, setAlias ] = useState('')
-  // const { data, error, isLoading } = useGetScoutByAliasQuery(alias)
+  const [ addedSailor, setAddedSailor ] = useState('')
+  const { data, error, isLoading } = useGetScoutByAliasQuery(alias)
+  // const { data, error, isLoading } = useAddScoutQuery()
 
-  const { data, error, isLoading } = alias ? useGetScoutByAliasQuery(alias)
-    : {
-      data: null,
-      error: null,
-      isLoading: false
-    }
+
   const handleChange = (event) => {
     setAlias(event.target.value)
   }
 
   const handleSubmit = (event) => {
-    console.log('An alias was submitted: ' + alias)
-    event.preventDefault
+    alert('An alias was submitted: ' + alias)
+    event.preventDefault()
+  }
+
+  const handleAddChange = (event) => {
+    setAddedSailor(event.target.value)
+  }
+
+  const handleAddSubmit = (event) => {
+    alert('A sailor scout was added: ' + addedSailor)
+    event.preventDefault()
   }
 
   return (
@@ -36,8 +42,8 @@ export default function Form () {
           </label><br/>
           <input type='submit' value='Submit' />
         </form>
-        {/* Adjust the code below to render to design.
-        It is working but we need it to render when we want it to not immediately */}
+        {/* Bug: The form fetches the input before clicking submit button
+          and so the error message displays until an input is entered */}
         {
           error ? (
             <>Oh no, there was an error</>
@@ -52,15 +58,44 @@ export default function Form () {
             </>
           ): null
         }
+      </div>
+      <div>
+        <h2 id='getData'>Add a Sailor Scout:</h2>
+        <form onSubmit={handleAddSubmit}>
+          <label>Add Alias:
+            <input
+              type='text'
+              value={addedSailor}
+              placeholder='Enter sailor scout'
+              onChange={handleAddChange}
+            />
+            <br/>
+          </label><br/>
+          <label>Add Name:
+            <input
+              type='text'
+              placeholder='Enter a sailor&apos;s alias'
+            />
+            <br/>
+          </label><br/>
+          <label>Add Alignment:
+            <input
+              type='text'
+              placeholder='Enter a sailor&apos;s affiliation'
+            />
+            <br/>
+          </label><br/>
+          <input type='submit' value='Submit' />
+        </form>
+      </div>
+      <div>
         <h3> Next tasks:</h3>
         <ul>
           <li>completed - Create a form in React</li>
           <li>completed - Create a Form that takes the input to fetch from your sailor api</li>
-          <li> create a field to add a new sailor</li>
-          <li> create a field to modify an existing sailor</li>
+          <li>in progress - create a field to add a new sailor</li>
           <li> |_ then fetch the newly modified data directly from the modification mentioned above</li>
         </ul>
-        <h3> Stretch goal is get a gif from giphy</h3>
       </div>
     </div>
   )
