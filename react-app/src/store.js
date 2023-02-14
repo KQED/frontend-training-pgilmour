d0d7e7296cffa67c7af7813992ee039370ade0a9
Pamela Gilmour <pgilmour@kqed.org>
2023-02-13 16:50:22 -0800

    fix(ksm-3865, Redux fetch): Remove RTKQ related code

--- react-app/src/store.js
+++ react-app/src/store.js
@@ -1,18 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit'
 import hexcodeReducer from './slices/hexPickerSlice'
+import scoutFormReducer from './slices/sailorSlice'
 import { setupListeners } from '@reduxjs/toolkit/query'
-import { sailorScoutApi } from './services/sailorScout'
+// import { sailorScoutApi } from './services/sailorScout'
 
 export const store = configureStore({
   reducer: {
     // Add the generated reducer as a specific top-level slice
-    [sailorScoutApi.reducerPath]: sailorScoutApi.reducer,
-    hexPicker: hexcodeReducer
+    // [sailorScoutApi.reducerPath]: sailorScoutApi.reducer,
+    hexPicker: hexcodeReducer,
+    scoutForm: scoutFormReducer
   },
   // Adding the api middleware enables caching, invalidation, polling,
   // and other useful features of `rtk-query`.
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat([sailorScoutApi.middleware])
+  // middleware: (getDefaultMiddleware) =>
+  //   getDefaultMiddleware().concat([sailorScoutApi.middleware])
 })
 
 // optional, but required for refetchOnFocus/refetchOnReconnect behaviors
