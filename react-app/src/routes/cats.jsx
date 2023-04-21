9f7222a4dc0fcb5c8dca532ce9d6528aa6e405ed
Pamela Gilmour <pgilmour@kqed.org>
2023-04-21 16:22:18 -0700

    fix(ksm-3153): Renaming components and code clean up

--- react-app/src/routes/cats.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Cats () {
-  return (
-    <div>
-      <p>This is the Cats route.</p>
-    </div>
-  )
-}
--- react-app/src/routes/root.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Root () {
-  return (
-    <div>
-      <h1>Hello From the ~root.jsx!</h1>
-    </div>
-  )
-}
