62c355ca4ec00ccbb1e51832627bf56cfc4ec2f8
Pamela Gilmour <pgilmour@kqed.org>
2023-04-21 17:00:39 -0700

    fix(ksm-3153, Set up Routes and Sass): Restore server/index.html to previous state

--- server/index.js
+++ server/index.js
@@ -4,8 +4,8 @@ const app = express()
 const path = require('path')
 const port = 3000
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname), '../dist', 'index.html')
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname), '../dist/index.html')
 })
 
 app.use(express.static(path.join(__dirname, '../dist')))
