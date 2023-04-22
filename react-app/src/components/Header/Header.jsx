c47deba2de4d901ca9a4f2af4b96d6328ca69f4d
Pamela Gilmour <pgilmour@kqed.org>
2023-04-21 17:06:11 -0700

    fix(ksm-3153, Set up Routes and Sass): Give the header a navbar

--- react-app/src/components/Header/Header.jsx
+++ react-app/src/components/Header/Header.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import styles from './Header.scss'
 
 export const Header = () => {
   return (
     <div className={styles.rootHeader}>
       <h1 className={styles.hello}>KQED Initial Training Project - Pamela Gilmour</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='cats'>Click for Cats</Link>
+          </li>
+          <li>
+            <Link to='hex-picker'>Click for Hex Picker Page</Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   )
 }
