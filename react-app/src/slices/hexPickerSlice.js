47a54c4056f48375258affe14293106d851575a2
Pamela Gilmour <pgilmour@kqed.org>
2023-05-19 15:28:37 -0700

    feat(ksm-3865, Add Redux): Create a directory for slices and put the store.js in there too

---
+++ react-app/src/slices/hexPickerSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+// In a future ticket, we will have the api create the hex code and deliver it to the frontend
+
+export const hexPickerSlice = createSlice({
+  name: 'hexPicker',
+  initialState: {
+    valueRGB: `rgb(${Math.floor(Math.random() * 256)},
+    ${Math.floor(Math.random() * 256)},
+    ${Math.floor(Math.random() * 256)})`
+  },
+  reducers: {
+    randomize: (state) => {
+      state.valueRGB = `rgb(${Math.floor(Math.random() * 256)},
+        ${Math.floor(Math.random() * 256)},
+        ${Math.floor(Math.random() * 256)})`
+    }
+  }
+})
+
+// Action creators are generated for each case reducer fx
+export const { randomize } = hexPickerSlice.actions
+export default hexPickerSlice.reducer
