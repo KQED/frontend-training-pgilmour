7c8b999895f4c8afb9a1de85da31f1091731c0a5
Pamela Gilmour <pgilmour@kqed.org>
2023-05-23 11:26:07 -0700

    feat(ksm-3865, Add Redux for api call): - REstucture the way state is updated in the reducers - Rewrite the fetch so it can call dispatch

--- react-app/src/slices/sailorsSlice.js
+++ react-app/src/slices/sailorsSlice.js
@@ -8,65 +8,65 @@ export const sailorsSlice = createSlice({
   reducers: {
     receiveAllSailors: (state, action) => {
       const data = action.payload
-      state['allSailors'] = {
-        data: data,
-        isLoading: false,
-        error: null
-      }
-      console.log(data)
+      state.data = data
+      state.isLoading = false
+      state.error = null
     },
     setIsLoading: (state) => {
-      state['allSailors'] = {
-        data: null,
-        isLoading: true,
-        error: null
-      }
+      state.data = null
+      state.isLoading = true
+      state.error = null
+    },
+    setError: (state, action) => {
+      const error = action.payload
+      state.data = null
+      state.isLoading = false
+      state.error = error
     },
   }
 })
 
-export const { receiveAllSailors, setIsLoading } = sailorsSlice.actions
+export const {
+  receiveAllSailors,
+  setIsLoading,
+  setError
+} = sailorsSlice.actions
 
-export const fetchAllSailors = (dispatch) => {
+export const fetchAllSailors = () => {
+  return (dispatch) => { // pass diapatch function as an arg
+    dispatch(setIsLoading())
 
-  dispatch(setIsLoading())
-
-  const url = 'http://localhost:3001/sailors'
-  const requestOptions = {
-    // method specifies HTTP method to be used, usually required
-    method: 'GET',
-    // headers provide more info about the request
-    // can do a deep dive on headers, especially with POST
-    headers: {
-      'Content-Type': 'application/json'
+    const url = 'http://localhost:3001/sailors'
+    const requestOptions = {
+      // method specifies HTTP method to be used, usually required
+      method: 'GET',
+      // headers provide more info about the request
+      // can do a deep dive on headers, especially with POST
+      headers: {
+        'Content-Type': 'application/json'
+      }
     }
-  }
 
-  fetch(url, requestOptions)
-    // the response from the server is passed as an arg,
-    // the arrow fx calls .json() on the response
-    // .json() is an async method that returns another Promise
-    // it reads the response and parses it as json
-    // 
-    .then(
-      (response) => {
-        if(response.ok) {
-          return response.json()
+    fetch(url, requestOptions)
+      .then(
+        (response) => {
+          if(response.ok) {
+            // console.log('response from first then in the fetch', response)
+            return response.json()
+          }
+        },
+        (error) => {
+          console.warn('Fetch error:', error)
+          // dispatch(setError(error))
         }
-      },
-      (error) => {
-        console.log('Fetch error:', error) // change to console.warn
-        // Add dispatch Error
-      }
-    )
-    .then(
-      (response) => {
-        console.log(response)
-        dispatch(receiveAllSailors({
-          allSailors: response,
-        }))
-      }
-    )
+      )
+      .then(
+        (response) => {
+          console.log('response from second then in the fetch', response)
+          dispatch(receiveAllSailors({ allSailors: response }))
+        }
+      )
+  }
 }
 
 export default sailorsSlice.reducer
