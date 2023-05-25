06742c8854669722fcb1df16fb40f269b280a668
Pamela Gilmour <pgilmour@kqed.org>
2023-05-25 14:13:15 -0700

    feat(ksm-3865, Add Redux for fetch): - Created a new property in initialState named oppositeValueRGB - Created a new reducer that takes the exisiting valueRGB string, splits it and processes it to generate an opposite RGB value

--- react-app/src/slices/hexPickerSlice.js
+++ react-app/src/slices/hexPickerSlice.js
@@ -6,18 +6,38 @@ export const hexPickerSlice = createSlice({
   name: 'hexPicker',
   initialState: {
     valueRGB: `rgb(${Math.floor(Math.random() * 256)},
-    ${Math.floor(Math.random() * 256)},
-    ${Math.floor(Math.random() * 256)})`
+      ${Math.floor(Math.random() * 256)},
+      ${Math.floor(Math.random() * 256)}
+    )`,
+    oppositeValueRGB: ''
   },
   reducers: {
     randomize: (state) => {
       state.valueRGB = `rgb(${Math.floor(Math.random() * 256)},
         ${Math.floor(Math.random() * 256)},
-        ${Math.floor(Math.random() * 256)})`
+        ${Math.floor(Math.random() * 256)}
+      )`
+    },
+    // create a reducer that generates a value
+    // depending on the state.valueRGB from the randomize reducer, seen above 
+    oppositeColor: (state) => {
+      // Take the state.valueRGB, split the values at the commas
+      // and put them into an array using map
+      const oppositeColor = state.valueRGB.split(',').map((item) => {
+        // process the strings
+        // the match method uses regex to retrive a matching result
+        // here, `\d+` is a regex expression symbolizing one or more consecutive digits
+        // the back slashes are delimiters symbolizing the beginning and end of a pattern
+        const value = parseInt(item.match(/\d+/)[0])
+        const oppositeValue = 255 - value
+        return oppositeValue.toString()
+      })
+
+      console.log('From oppositeColor reducer:', oppositeColor)
+      state.oppositeValueRGB = `rgb(${oppositeColor.join(',')})` // joins the values in the map array
     }
   }
 })
 
-// Action creators are generated for each case reducer fx
-export const { randomize } = hexPickerSlice.actions
+export const { randomize, oppositeColor } = hexPickerSlice.actions
 export default hexPickerSlice.reducer
