ac6626d9183901947b712e2ce835cbf7bf543b48
Pamela Gilmour <pgilmour@kqed.org>
2023-05-25 14:35:13 -0700

    feat(ksm-3865, Add Redux for fetch): Add new opposite rgb value to the HexPicker component

--- react-app/src/components/HexPicker/HexPicker.jsx
+++ react-app/src/components/HexPicker/HexPicker.jsx
@@ -1,15 +1,29 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { randomize } from '../../slices/hexPickerSlice'
+import {
+  randomize,
+  oppositeColor
+} from '../../slices/hexPickerSlice'
 import style from './HexPicker.scss'
 
 export default function HexPicker () {
   const generatedValue = useSelector((state) => state.hexPicker.valueRGB)
+  const fontValue = useSelector((state) => state.hexPicker.oppositeValueRGB)
+
   const dispatch = useDispatch()
+
   const applyColor = () => {
     document.body.style.background = generatedValue
   }
+  const applyFontColor = () => {
+    document.body.style.color = fontValue
+  }
+
+  // console.log('generatedValue:', generatedValue)
+  // console.log('fontValue:', fontValue)
   applyColor()
+  applyFontColor()
+
   return (
     <div className={style}>
       <h2>Random RGB Generator</h2>
@@ -17,11 +31,17 @@ export default function HexPicker () {
         <p>{generatedValue}</p>
         <button
           aria-label='Generate random RGB value'
-          onClick={() => dispatch(randomize())}
+          onClick={() => {
+            dispatch(randomize())
+            dispatch(oppositeColor())
+          }
+          }
         >
           Click to Generate a Random RBG Value
         </button>
       </section>
+      <h2>Opposite RGB Value</h2>
+      <p>{fontValue}</p>
     </div>
   )
 }
