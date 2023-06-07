d597f8c74c5db1d14747c96d5de1cb1734555693
Pamela Gilmour <pgilmour@kqed.org>
2023-06-07 12:15:52 -0700

    feat(ksm-3154, Add SSR): Revert to using createRoot, hydrate is deprecated

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,24 +1,14 @@
 import React from 'react'
-// import { createRoot } from 'react-dom/client'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import store from './slices/store'
 import App from './App'
 
-// const rootElement = document.getElementById('root')
-// const root = createRoot(rootElement)
+const rootElement = document.getElementById('root')
+const root = createRoot(rootElement)
 
-// root.render(
-//   <Provider store={store}>
-//     <App />
-//   </Provider>
-// )
-
-ReactDOM.hydrate(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
 )
