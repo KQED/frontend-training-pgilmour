ba3601a3d1878ea0a92e14bb38bde78da6616756
Pamela Gilmour <pgilmour@kqed.org>
2023-06-08 12:12:18 -0700

    feat(ksm-3154, Add SSR): Pair down the ssr server

--- servers/server.ssr.js
+++ servers/server.ssr.js
@@ -1,40 +1,22 @@
 const express = require('express')
-const { StrictMode } = require('react')
 const React = require('react')
 const ReactDOMServer = require('react-dom/server')
-const { Provider } = require('react-redux')
-const store = require('../react-app/src/slices/store')
 
-const App = require('../react-app/src/App').default
+const App = require('../react-app/src/App.js').default
 
 const app = express()
 const PORT = process.env.PORT || 3031
 
-app.get('/', (req, res) => {
+app.use(express.static('dist'))
 
-  const app = (
-    <StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </StrictMode>
+app.get('*', (req, res) => {
+
+  const content = ReactDOMServer.renderToString(
+    <App />
   )
-  const content = ReactDOMServer.renderToString(app)
-  const html = `
-    <!DOCTYPE html>
-    <html lang="en">
-      <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>React SSR</title>
-      </head>
-      <body>
-        <div id="root">${content}</div>
-      </body>
-    </html>
-  `
 
-  res.send(html)
+  res.send(content)
+
 })
 
 app.listen(PORT, () => {
