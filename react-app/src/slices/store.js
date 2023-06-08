7ed981e434f0c3dc1b25a245a43787e899c2d9c1
Pamela Gilmour <pgilmour@kqed.org>
2023-06-08 12:09:58 -0700

    feat(ksm-3154, Add SSR): Break out the reducers in the global store

--- react-app/src/slices/store.js
+++ react-app/src/slices/store.js
@@ -2,10 +2,19 @@ import { configureStore } from '@reduxjs/toolkit'
 import hexCodeReducer from './hexPickerSlice'
 import sailorsReducer from './sailorsSlice'
 
-export default configureStore({
-  reducer: {
-    hexPicker: hexCodeReducer,
-    sailors: sailorsReducer
-  },
+const rootReducer = {
+  hexPicker: hexCodeReducer,
+  sailors: sailorsReducer
+}
+
+const store = configureStore({
+// export default configureStore({
+  // reducer: {
+  //   hexPicker: hexCodeReducer,
+  //   sailors: sailorsReducer
+  // },
+  reducer: rootReducer,
   devTools: true
 })
+
+export { store, rootReducer }
